a66e4fba1f7b0cb9755fff9090a290b679becf23
Danielo64 <danyjogador64@gmail.com>
2021-09-03 17:06:34 -0300

    deu certo família :)

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,31 @@
 import db from './db.js';
 import express from 'express'
 import cors from 'cors'
+import crypto from 'crypto-js'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.post('/login', async (req, resp) => {
+    const login = req.body.login;
+    const senha = req.body.senha;
+    const crytoSenha = crypto.SHA256(senha).toString(crypto.enc.Base64);
+
+    let u = await db.tb_usuario.findOne({
+        where: { 
+            ds_login: login, 
+            ds_senha: crytoSenha 
+        },
+        raw: true
+    });
+
+    if (u == null)
+        return resp.send({ erro: 'Credenciais inválidas' });
+
+    delete u.ds_senha;
+    resp.sendStatus(u);
+});
 
 
 app.post('/sala', async (req, resp) => {
@@ -44,7 +65,9 @@ app.post('/usuario', async (req, resp) => {
             return resp.send({ erro: 'Usuário já existe!' });
         
         let r = await db.tb_usuario.create({
-            nm_usuario: usuParam.nome
+            nm_usuario: usuParam.nome,
+            ds_login: usuParam.login,
+            ds_senha: crypto.SHA256(usuParam.senha).toString(crypto.enc.Base64)
         })
         resp.send(r);
     } catch (e) {
@@ -116,7 +139,14 @@ app.get('/chat/:sala', async (req, resp) => {
     }
 })
 
-
+app.delete('/chat/:id', async (req, resp) => {
+    try {
+        let r = await db.tb_chat.destroy({ where: { id_chat: req.params.id } })
+        resp.sendStatus(200);
+    } catch (e) {
+        resp.send({ erro: e.toString() });
+    }
+})
 
 app.listen(process.env.PORT,
            x => console.log(`>> Server up at port ${process.env.PORT}`))
